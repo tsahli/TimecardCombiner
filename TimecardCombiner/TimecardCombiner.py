a52ed72868c35c731bac3f2dea75a2543295a6e6
Taylor Sahli <taylorsahli837@gmail.com>
2019-12-16 13:58:26 -0700

    Reads all .pdf files in a directory recursively

--- TimecardCombiner/TimecardCombiner.py
+++ TimecardCombiner/TimecardCombiner.py
@@ -1,5 +1,6 @@
 import tika
 from tika import parser
+import os
 
 def dictMachine(item):
     split = item.split(":")
@@ -22,11 +23,6 @@ def tupleMachine(row):
                 jobNumber = row[index].get(key)
     return stTotal, otTotal, workOrderNum, jobNumber
 
-timecard = parser.from_file('Time Card Form.pdf')
-timecardContent = timecard['content']
-split = timecardContent.split('\n')
-split.sort()
-
 row1 = []
 row2 = []
 row3 = []
@@ -35,36 +31,46 @@ row5 = []
 row6 = []
 row7 = []
 
-for item in split:
-    item = item.strip('\t')
-    if item:
-        if item.startswith('EmployeeNumber:'):
-            split = item.split(' ')
-            employeeNumber = split[1]
-        if item.startswith('WeekEnding:'):
-            split = item.split(' ')
-            weekEnding = split[1]
-        if item.startswith('1-'):
-            row1.append(dictMachine(item))
-        if item.startswith('2-'):
-            row2.append(dictMachine(item))
-        if item.startswith('3-'):
-            row3.append(dictMachine(item))
-        if item.startswith('4-'):
-            row4.append(dictMachine(item))
-        if item.startswith('5-'):
-            row5.append(dictMachine(item))
-        if item.startswith('6-'):
-            row6.append(dictMachine(item)) 
-        if item.startswith('7-'):
-            row7.append(dictMachine(item))
+timecardDirectory = os.getcwd()
 
-# STTOTAL, OTTOTAL, WORKORDERNUM, JOBNUM
-row1Tuple = tupleMachine(row1)
-row2Tuple = tupleMachine(row2)
-row3Tuple = tupleMachine(row3)
-row4Tuple = tupleMachine(row4)
-row5Tuple = tupleMachine(row5)
-row6Tuple = tupleMachine(row6)
-row7Tuple = tupleMachine(row7)
+for file in os.listdir(timecardDirectory):
+    if file.endswith('.pdf') and 'PROCESSED' not in file: # NEED TO APPEND 'PROCESSED' TO FILENAME AFTER PROCESSING
+        timecard = parser.from_file(file)
+        timecardContent = timecard['content']
+        split = timecardContent.split('\n')
+        split.sort()
 
+        for item in split:
+            item = item.strip('\t')
+            if item:
+                if item.startswith('EmployeeNumber:'):
+                    split = item.split(' ')
+                    employeeNumber = split[1]
+                if item.startswith('WeekEnding:'):
+                    split = item.split(' ')
+                    weekEnding = split[1]
+                if item.startswith('1-'):
+                    row1.append(dictMachine(item))
+                if item.startswith('2-'):
+                    row2.append(dictMachine(item))
+                if item.startswith('3-'):
+                    row3.append(dictMachine(item))
+                if item.startswith('4-'):
+                    row4.append(dictMachine(item))
+                if item.startswith('5-'):
+                    row5.append(dictMachine(item))
+                if item.startswith('6-'):
+                    row6.append(dictMachine(item))
+                if item.startswith('7-'):
+                    row7.append(dictMachine(item))
+
+        # STTOTAL, OTTOTAL, WORKORDERNUM, JOBNUM
+        row1Tuple = tupleMachine(row1)
+        row2Tuple = tupleMachine(row2)
+        row3Tuple = tupleMachine(row3)
+        row4Tuple = tupleMachine(row4)
+        row5Tuple = tupleMachine(row5)
+        row6Tuple = tupleMachine(row6)
+        row7Tuple = tupleMachine(row7)
+        
+        print(row1Tuple)
